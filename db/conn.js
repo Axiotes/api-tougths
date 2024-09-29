3481bbc9af3049c08bd0317463196d69cc4e953f
Arthur Axiotes <axiotesarthur@gmail.com>
2024-09-29 19:41:50 -0300

    fix: adjustment in the .env file to connect to the database

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_NAME, process.env.PASSWORD, {
   host: process.env.HOST,
   dialect: "mysql",
   port: process.env.PORT,
